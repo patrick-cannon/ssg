6093926cd1f7d7514fec48b1fdcc2256fa7b0781
Patrick Cannon <cannonpatrickw@gmail.com>
2025-02-13 16:00:55 -0600

    more work on the split nodes delimiter

--- src/inline.py
+++ src/inline.py
@@ -1,2 +1,28 @@
+from textnode import TextNode, TextType
+
 # list of "old_nodes", return list of "new_nodes"
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node != TextType.NORMAL_TEXT:
+            new_nodes.append(node)
+            continue
+        split_node = []
+        section = node.text.split(delimiter)
+        if len(section) % 2 == 0:
+            raise Exception("invalid Markdown syntax (missing closing delimiter)")
+        for i in range(len(section)):
+            if section[i] == "":
+                continue
+
+            if i % 2 == 0:
+                split_node.append(
+                    TextNode(text = section[i], text_type = TextType.NORMAL_TEXT)
+                )
+            else:
+                split_node.append(
+                    TextNode(text = section[i], text_type = text_type)
+                )
+
+        new_nodes.extend(split_node)
+    return new_nodes
