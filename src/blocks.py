2212275558416ed8cfaacc3477eb1f22f8c45cf2
Patrick Cannon <cannonpatrickw@gmail.com>
2025-02-19 20:14:50 -0600

    began work on blocktypes

--- src/blocks.py
+++ src/blocks.py
@@ -10,3 +10,15 @@ def markdown_to_blocks(markdown):
         cleaned_block_strings.append(stripped_block)
 
     return cleaned_block_strings
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(markdown):
+    if
